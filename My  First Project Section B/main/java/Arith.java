efbc62cda416f35914f27eac540f91b73363cc8b
Ishika Dubey <ishikadubey2020@gmail.com>
2026-04-20 22:11:32 +0530

    Add Eclipse workspace projects: Constructors, Encapsulation, Inheritance, Interfaces, Abstract Classes, Exception Handling, Wrapper Classes, and more

---
+++ My  First Project Section B/main/java/Arith.java
@@ -0,0 +1,15 @@
+
+public class Arith {
+
+	public static void main(String[] args) {
+	      int a= 10;
+	      int b = 5;
+	      int c = a+b;
+	      System.out.println(" Addition is :"+c);
+	      int d = a-b;
+	      System.out.println(" Subtraction is : "+d);
+	      
+
+	}
+
+}
